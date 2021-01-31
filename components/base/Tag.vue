b12109377348af4479f87411a3fda1038c6fff36
Jip Fr <jipfrijlink@gmail.com>
2021-01-31 19:22:11 +0100

    Fix light theme for tags

--- components/base/Tag.vue
+++ components/base/Tag.vue
@@ -5,11 +5,12 @@
 <style lang="scss" scoped>
 .tag {
   padding: 3px 8px;
-  background: rgba(0, 0, 0, 0.1);
+  background: rgba(0, 0, 0, 0.05);
   display: inline-flex;
   align-items: center;
   margin-right: 3px;
   font-size: 0.9rem;
+  border-radius: 6px;
 }
 .tag::before {
   content: '';
@@ -17,14 +18,16 @@
   width: 0.5rem;
   height: 0.5rem;
   border-radius: 50%;
-  background: rgba(255, 255, 255, 0.2);
+  background: rgba(0, 0, 0, 0.2);
   margin-right: 5px;
 }
 
 @media (prefers-color-scheme: dark) {
   .tag {
     background: rgba(255, 255, 255, 0.1);
-    border-radius: 6px;
+  }
+  .tag::before {
+    background: rgba(255, 255, 255, 0.2);
   }
 }
 </style>
