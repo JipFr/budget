9d5e20c8df6866a1b0ee7ddc28de3000adb236ee
Jip Fr <jipfrijlink@gmail.com>
2021-02-03 13:58:04 +0100

    Re-style tags

--- components/base/Tag.vue
+++ components/base/Tag.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="tag">{{ capitalize }}</div>
+  <div class="tag" :style="`--bg-color: ${color}`">{{ capitalize }}</div>
 </template>
 
 <style lang="scss" scoped>
 .tag {
-  padding: 3px 8px;
-  background: rgba(0, 0, 0, 0.05);
+  padding: 0;
+  // background: rgba(0, 0, 0, 0.014);
   display: inline-flex;
   align-items: center;
-  margin-right: 3px;
+  margin-right: 15px;
+  margin-top: 2px;
   font-size: 0.9rem;
   border-radius: 6px;
 }
@@ -18,8 +19,8 @@
   width: 0.5rem;
   height: 0.5rem;
   border-radius: 50%;
-  background: rgba(0, 0, 0, 0.2);
-  margin-right: 5px;
+  background: var(--bg-color, rgba(0, 0, 0, 0.2));
+  margin-right: 8px;
 }
 
 @media (prefers-color-scheme: dark) {
@@ -39,6 +40,16 @@ export default {
       type: String,
       required: true,
     },
+  },
+  data() {
+    // Get the tag's color, as assigned in `user.js`
+    const colors = this.$store.state.user.tagColors
+    const tagName = this.tag.toString().trim().toLowerCase()
+    const color = colors[tagName]
+    return {
+      colors,
+      color,
+    }
   },
   computed: {
     capitalize() {
--- store/user.js
+++ store/user.js
@@ -4,6 +4,7 @@ export const state = () => ({
     transactionsInPeriod: [],
     loading: true,
   },
+  tagColors: {},
   from: '2019-01-01',
   until: '2022-01-01',
 })
@@ -14,6 +15,7 @@ function applyFilter(state) {
   const untilDate = new Date(state.until)
   const hour = 1e3 * 60 * 60
 
+  // Filter transactions by date
   state.data.transactionsInPeriod = state.data.transactions.filter((v) => {
     const d = new Date(v.date)
     return (
@@ -21,6 +23,37 @@ function applyFilter(state) {
       d.getTime() <= untilDate.getTime() + hour
     )
   })
+
+  // Assign color for each tag
+  // Get unique tags
+  const allTags = [
+    ...new Set(
+      state.data.transactions
+        .map((v) => v.categories.map((v) => v.trim().toLowerCase()))
+        .flat()
+    ),
+  ]
+
+  // Fixed colors
+  const colors = [
+    'rgb(0, 122, 255)',
+    'rgb(52, 199, 89)',
+    'rgb(255, 149, 0)',
+    'rgb(88, 86, 214)',
+    'rgb(255, 45, 85)',
+    'rgb(175, 82, 222)',
+    'rgb(255, 59, 48)',
+    'rgb(90, 200, 250)',
+    'rgb(255, 204, 0)',
+  ]
+
+  // Assign color to each tag
+  // const fullRange = 360
+  for (const [i, name] of Object.entries(allTags)) {
+    // const c = (Number(i) / allTags.length) * fullRange
+    // state.tagColors[name] = `hsl(${c}, 100%, 50%)`
+    state.tagColors[name] = colors[i % colors.length]
+  }
 }
 
 export const mutations = {
