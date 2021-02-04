2e48e715d5404c7b159e14cf569de530d7cc44a4
Jip Fr <jipfrijlink@gmail.com>
2021-02-04 13:06:18 +0100

    Fix rounding issue

--- components/base/CategoryOverview.vue
+++ components/base/CategoryOverview.vue
@@ -7,7 +7,7 @@
           <span class="do-space-x">
             <money :cents="category[1].gained" />
             <money class="negative" :cents="category[1].spent * -1" />
-            ({{ category[1].spentPercentage }}%)
+            ({{ Math.round(category[1].spentPercentage) }}%)
           </span>
         </div>
         <div
