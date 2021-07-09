6417176bc22f063b99305c357f4d02f4e8d36ffe
Jip Fr <jipfrijlink@gmail.com>
2021-07-09 22:10:24 +0200

    feat(banner): message type banner

--- components/base/Banner.vue
+++ components/base/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="banner">
+  <div class="banner" :data-type="type">
     <slot />
   </div>
 </template>
@@ -10,5 +10,22 @@
   border-radius: 3px;
   border-left: 4px solid var(--negative);
   background: rgba(255, 0, 0, 0.1);
+
+  &[data-type='message'] {
+    border-left-color: var(--anchor);
+    background: rgba(0, 200, 255, 0.1);
+  }
 }
 </style>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: false,
+      default: 'error',
+    },
+  },
+}
+</script>
