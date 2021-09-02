1b5091c36bb8b8a62207e975a6faaedac9ce09de
Jip Fr <jipfrijlink@gmail.com>
2021-09-02 16:45:20 +0200

    chore: remove unused methods on pages

--- pages/overview.vue
+++ pages/overview.vue
@@ -11,21 +11,10 @@ export default {
   components: {
     TotalOverview,
   },
-
   computed: {
-    getPayments() {
-      const user = this.$store.state.user.data
-      return user.transactionsInPeriod || []
-    },
     getAllPayments() {
       const user = this.$store.state.user.data
       return user.transactions || []
-    },
-    isLoading() {
-      return this.$store.state.user.data.loading
-    },
-    getQuery() {
-      return Object.keys(this.$route.query).join('')
     },
   },
 }
--- pages/recurring.vue
+++ pages/recurring.vue
@@ -13,19 +13,9 @@ export default {
   },
 
   computed: {
-    getPayments() {
-      const user = this.$store.state.user.data
-      return user.transactionsInPeriod || []
-    },
     getAllPayments() {
       const user = this.$store.state.user.data
       return user.transactions || []
-    },
-    isLoading() {
-      return this.$store.state.user.data.loading
-    },
-    getQuery() {
-      return Object.keys(this.$route.query).join('')
     },
   },
 }
