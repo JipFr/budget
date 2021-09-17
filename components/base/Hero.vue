2f445e7cfb92a66353135c0646f4c50bad6dd5c5
Jip Fr <jipfrijlink@gmail.com>
2021-09-18 01:01:32 +0200

    feat(hero): change hero cards a bit

--- components/base/Hero.vue
+++ components/base/Hero.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="hero" :class="isLoading ? 'loading' : ''">
     <div class="hero-layout">
-      <card class="fw highlight">
+      <card class="highlight" :class="foodTotal <= 0 ? 'fw' : ''">
         <subtitle>In this period...</subtitle>
-        <h1>
+        <h2 v-if="foodTotal > 0">
+          <money :cents="regularTotal" />
+        </h2>
+        <h1 v-else>
           <money :cents="regularTotal" />
         </h1>
       </card>
-      <card v-if="foodTotal > 0 || isLoading" class="fw">
-        <subtitle>Food remaining (of <money :cents="foodTotal" />)</subtitle>
-        <h1>
-          <money :cents="foodTotal - foodSpent" />
-        </h1>
+      <card v-if="foodTotal > 0">
+        <subtitle>Total in period</subtitle>
+        <h2>
+          <money :cents="gained - spent" />
+        </h2>
       </card>
-      <card>
+      <card v-if="foodTotal > 0" class="fw">
+        <subtitle>Food remaining (of <money :cents="foodTotal" />)</subtitle>
+        <h2>
+          <money :cents="foodTotal - foodSpent" />
+        </h2>
+      </card>
+
+      <card v-if="foodTotal <= 0">
         <subtitle>Gained</subtitle>
         <h2>
           <money :cents="gained" />
         </h2>
       </card>
-      <card>
+      <card v-if="foodTotal <= 0">
         <subtitle>Spent</subtitle>
         <h2>
           <money :cents="spent" />
