43a3ace62dfc38bd0faed96ae09dce828878481f
Jip Fr <jipfrijlink@gmail.com>
2021-09-18 19:27:09 +0200

    fix(categories): fix being unable to edit transactions from "transactions in category" list

--- components/base/PaymentCard.vue
+++ components/base/PaymentCard.vue
@@ -26,7 +26,7 @@
         <span v-else class="bold usual-description">
           {{ description }}
         </span>
-        <span v-if="typeof payment.inXDays !== 'undefined'" class="in-x-days">
+        <span v-if="isInXDays(payment)" class="in-x-days">
           <span v-if="payment.inXDays === 0"> Today </span>
           <span v-else-if="payment.inXDays === 1"> Tomorrow </span>
           <span v-else>
@@ -277,6 +277,9 @@ export default {
     },
     doEdit() {
       this.$nuxt.$emit('edit-transaction', this.payment.id)
+    },
+    isInXDays(payment) {
+      return typeof payment.inXDays !== 'undefined'
     },
   },
 }
--- pages/categories.vue
+++ pages/categories.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <category-overview :payments="getPayments" />
+    <portal to="right-side">
+      <div class="new-transaction-content">
+        <new-transaction-wrapper :show-button="false" class="no-desktop-bg" />
+      </div>
+    </portal>
   </div>
 </template>
 
 <script>
+import { Portal } from 'portal-vue'
 import CategoryOverview from '~/components/base/CategoryOverview'
+import NewTransactionWrapper from '~/components/new-transaction/MainWrapper'
 
 export default {
   components: {
     CategoryOverview,
+    Portal,
+    NewTransactionWrapper,
   },
   computed: {
     getPayments() {
