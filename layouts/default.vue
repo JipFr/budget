707848cda1d48c7845961cf7c8861c63017e0f67
Jip Fr <jipfrijlink@gmail.com>
2021-11-24 11:46:07 +0100

    fix(period): fix period start being static

--- layouts/default.vue
+++ layouts/default.vue
@@ -246,7 +246,26 @@ export default {
   },
   async fetch() {
     if (!this.hasFetched) {
+      const day = 1e3 * 60 * 60 * 24
+      const d = new Date()
+
+      // Find last month's "23rd" and this month's "23rd"
+      // We're treating the 23rd as the start of the month — that's when I get paid.
+      const currentMonth23rd = new Date(d)
+      currentMonth23rd.setDate(23)
+
+      const lastMonth23rd = new Date(
+        new Date(d.getFullYear(), d.getMonth()).getTime() - day * 0.75 // This is a bit whack, but TIMEZONES!
+      )
+      lastMonth23rd.setDate(23)
+
+      const fromDate =
+        Date.now() > currentMonth23rd.getTime()
+          ? currentMonth23rd
+          : lastMonth23rd
+
       this.setUntil(new Date().toISOString().split('T')[0])
+      this.setFrom(fromDate.toISOString().split('T')[0])
     }
 
     this.setLoading(true)
@@ -302,6 +321,7 @@ export default {
       setPerson: 'user/set',
       setLoading: 'user/setLoading',
       setUntil: 'user/setUntil',
+      setFrom: 'user/setFrom',
     }),
   },
 }
