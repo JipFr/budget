d841f184abb10b5916cbb0f7b37b1d247d7107fb
Jip Fr <jipfrijlink@gmail.com>
2022-02-23 18:37:49 +0100

    chore(dates): set "from" date to today on 23rd

--- layouts/default.vue
+++ layouts/default.vue
@@ -264,6 +264,8 @@ export default {
       // We're treating the 23rd as the start of the month — that's when I get paid.
       const currentMonth23rd = new Date(d)
       currentMonth23rd.setDate(23)
+      currentMonth23rd.setHours(3)
+      currentMonth23rd.setMinutes(0)
 
       const lastMonth23rd = new Date(
         new Date(d.getFullYear(), d.getMonth()).getTime() - day * 0.75 // This is a bit whack, but TIMEZONES!
