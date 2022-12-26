62915a06b8937e39c88f3bfd531ee320afe5df7f
Jip Fr <jipfrijlink@gmail.com>
2022-12-26 18:43:10 +0100

    style(changes): make grid auto-fit, not fill

--- components/base/PriceChanges.vue
+++ components/base/PriceChanges.vue
@@ -85,7 +85,7 @@ hr {
 .cards {
   display: grid;
   grid-gap: 10px;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 
   .expanded {
     grid-column: 1 / -1;
