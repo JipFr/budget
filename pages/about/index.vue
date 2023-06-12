e4cae14f689a4af3d38cb3c117754ea8df8eb53f
Jip Fr <jipfrijlink@gmail.com>
2023-06-12 13:55:32 +0200

    chore(plaid): remove plaid access tokens when logging out

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -73,6 +73,7 @@ export default {
       return str.slice(0, 1).toUpperCase() + str.slice(1)
     },
     async logout() {
+      localStorage.removeItems('plaid-tokens')
       await SupabaseClient.auth.signOut()
     },
   },
