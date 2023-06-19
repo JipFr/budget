10a22fb214ed309b9b1d63cb061fa72ab71fa341
Jip Fr <jipfrijlink@gmail.com>
2023-06-20 00:37:11 +0200

    feat(ah): create recipe parser

--- components/base/util/AhView.vue
+++ components/base/util/AhView.vue
@@ -102,6 +102,68 @@ export function ahFetch(path, opts) {
   ).then((d) => d.json())
 }
 
+function formatEur(str, between = '') {
+  return `€${between}${str.replace(/,/g, '.')}`
+}
+
+function receiptToDescription(receipt) {
+  const storeName = receipt
+    .find((t) => t.type === 'text')
+    .value.replace(/\d+/g, '')
+
+  const description = [storeName]
+
+  // Products
+  let productRange = receipt.slice(
+    receipt.findIndex((t) => t.type === 'products-header') + 2
+  ) // Starting from after the divider
+  productRange = productRange.slice(
+    0,
+    productRange.findIndex((t) => t.type === 'divider')
+  )
+  productRange = productRange.filter(
+    (t) =>
+      t.type === 'product' &&
+      !t.description.toLowerCase().includes('bonuskaart')
+  )
+
+  for (const product of productRange) {
+    const desc = product.description.toLowerCase()
+    const eur = formatEur(product.amount)
+
+    if (desc.startsWith('+')) {
+      description[description.length - 1] =
+        description[description.length - 1] + ` + ${eur}`
+    } else {
+      const descText = `${
+        product.quantity && Number(product.quantity) !== 1
+          ? product.quantity
+          : ''
+      } ${desc} ${eur}`.trim()
+      description.push(descText)
+    }
+  }
+
+  // Add "money removed" (statiegeld, bonus box)
+
+  const bonusLines = receipt.filter((t) => t.quantity === 'BONUS')
+  for (const bonusLine of bonusLines) {
+    description.push(
+      `${bonusLine.description.toLowerCase()} bonus ${formatEur(
+        bonusLine.amount
+      )}`
+    )
+  }
+
+  // Statiegeld
+  const deposit = receipt.find((t) => t.description === 'EMBALLAGE')
+  if (deposit) {
+    description.push(`Statiegeld ${formatEur(deposit.amount, '-')}`)
+  }
+
+  return description.map((t) => t.trim().replace(/ {2}/g, ' ')).join('\n')
+}
+
 export default {
   components: {
     Subtitle,
@@ -137,6 +199,20 @@ export default {
           Authorization: `Bearer ${token.access_token}`,
         },
       })
+
+      // ! THIS IS TEMPORARY!
+      const receipts = itemInfo.slice(0, 10)
+      for (const receipt of receipts) {
+        const receiptDetails = await ahFetch(
+          `/mobile-services/v1/receipts/${receipt.transactionId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token.access_token}`,
+            },
+          }
+        )
+        receiptToDescription(receiptDetails)
+      }
 
       accountsInfo.push({
         id: token.id,
