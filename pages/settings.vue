efda0992956a738c6ef1df9c690056a0c2ae3858
Jip Fr <jipfrijlink@gmail.com>
2023-06-20 22:57:51 +0200

    feat(plugins): add plugin manager to settings page

--- pages/settings.vue
+++ pages/settings.vue
@@ -9,6 +9,11 @@
     <page-title>Bank accounts</page-title>
 
     <plaid-view />
+
+    <div class="mt">
+      <page-title>Plugins</page-title>
+      <plugin-manager />
+    </div>
   </div>
 </template>
 
@@ -23,12 +28,14 @@
 import PageTitle from '~/components/title/PageTitle'
 import Profile from '~/components/base/util/Profile'
 import PlaidView from '~/components/base/util/PlaidView'
+import PluginManager from '~/components/base/util/PluginManager'
 
 export default {
   components: {
     PageTitle,
     Profile,
     PlaidView,
+    PluginManager,
   },
 }
 </script>
