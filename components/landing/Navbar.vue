2fac205a725013da8e2ac046f2ddf82e9bb31e16
Jip Fr <jipfrijlink@gmail.com>
2023-07-08 20:00:06 +0200

    feat(landing): add password field to login

--- components/landing/Navbar.vue
+++ components/landing/Navbar.vue
@@ -14,19 +14,29 @@
           <chevron-down-icon />
         </div>
         <div v-if="dropdownOpen" class="dropdown-out">
-          <div class="logos">
-            <button data-link="github" @click="login('github')">
-              <github-logo />
-              Log in with GitHub
-            </button>
-            <button data-link="discord" @click="login('discord')">
-              <discord-logo />
-              Log in with Discord
-            </button>
-            <button data-link="google" @click="login('google')">
-              <google-logo />
-              Log in with Google
-            </button>
+          <div v-if="canLogin">
+            <div class="logos">
+              <button data-link="github" @click="login('github')">
+                <github-logo />
+                Log in with GitHub
+              </button>
+              <button data-link="discord" @click="login('discord')">
+                <discord-logo />
+                Log in with Discord
+              </button>
+              <button data-link="google" @click="login('google')">
+                <google-logo />
+                Log in with Google
+              </button>
+            </div>
+          </div>
+          <div v-else>
+            <app-input
+              v-model="password"
+              type="password"
+              placeholder="krab1sc00l"
+              label="What's the magic word?"
+            />
           </div>
         </div>
       </div>
@@ -131,6 +141,7 @@ header {
 <script>
 // Import components
 import Logo from '~/components/base/util/Logo'
+import AppInput from '~/components/base/inputs/Input'
 
 // Import icons
 import ChevronDownIcon from '~/assets/icons/chevron-down.svg?inline'
@@ -147,6 +158,7 @@ import { getRedirectUrl } from '~/util/redirectUrl'
 export default {
   components: {
     Logo,
+    AppInput,
     ChevronDownIcon,
     GithubLogo,
     DiscordLogo,
@@ -155,7 +167,14 @@ export default {
   data() {
     return {
       dropdownOpen: false,
+      password: '',
     }
+  },
+  computed: {
+    canLogin() {
+      // Yes, this is client-side verifcation. I only care about the surface-level randos.
+      return this.password === 'prescience' // (I like Dune)
+    },
   },
   methods: {
     toggleDropdown() {
