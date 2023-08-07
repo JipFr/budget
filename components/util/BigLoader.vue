eaa8ba4904414139d2f6b9070ba5d49e4e223c8c
Jip Fr <jipfrijlink@gmail.com>
2023-08-07 11:49:11 +0200

    style: make big loader less tall

--- components/util/BigLoader.vue
+++ components/util/BigLoader.vue
@@ -5,27 +5,8 @@
 <style lang="scss" scoped>
 .big-loader {
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 80vh;
-  gap: 1rem;
-}
-.icon {
-  width: 8rem;
-  height: 8rem;
-  border-radius: 6px;
-}
-
-@keyframes slowJiggle {
-  0% {
-    transform: rotate(-5deg);
-  }
-  50% {
-    transform: rotate(5deg);
-  }
-  100% {
-    transform: rotate(-5deg);
-  }
+  height: 30vh;
 }
 </style>
