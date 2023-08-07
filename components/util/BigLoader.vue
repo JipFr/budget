e8cc072bc91766d511f56df46ee4c4424f2440c1
Jip Fr <jipfrijlink@gmail.com>
2023-08-07 11:30:13 +0200

    fix: fix "no transactions" before load

---
+++ components/util/BigLoader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="big-loader">
+    <krab-icon class="icon" />
+    Loading...
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.big-loader {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  gap: 1rem;
+}
+.icon {
+  width: 8rem;
+  height: 8rem;
+  border-radius: 6px;
+  filter: grayscale(0.2);
+  // animation: 3s slowJiggle infinite;
+}
+
+@keyframes slowJiggle {
+  0% {
+    transform: rotate(-5deg);
+  }
+  50% {
+    transform: rotate(5deg);
+  }
+  100% {
+    transform: rotate(-5deg);
+  }
+}
+</style>
+
+<script>
+import KrabIcon from '~/assets/logos/krab-full-pride.svg?inline'
+
+export default {
+  components: {
+    KrabIcon,
+  },
+}
+</script>
